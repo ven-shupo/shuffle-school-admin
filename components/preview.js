62cbee02fa4fedbe713e60d242372c9a582882af
opushnev <opushnev.ia@phystech.edu>
2024-01-28 18:55:02 +0300

    fix

--- components/preview.js
+++ components/preview.js
@@ -1,88 +1,63 @@
 import React, {useState} from 'react'
-import {useTelegramWeb} from "../lib/telegramWeb";
+// import {useTelegramWeb} from "../lib/telegramWeb";
 import styles from '../styles/Home.module.css';
-import { Table } from "./table";
-import { Modal } from "./modal";
+import { CompactTable } from '@table-library/react-table-library/compact';
+
+const nodes = [
+  {
+    id: '0',
+    name: 'Shopping List',
+    deadline: new Date(2020, 1, 15),
+    type: 'TASK',
+    isComplete: true,
+    nodes: 3,
+  },
+];
+
+const COLUMNS = [
+  { label: 'Task', renderCell: (item) => item.name },
+  {
+    label: 'Deadline',
+    renderCell: (item) =>
+      item.deadline.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      }),
+  },
+  { label: 'Type', renderCell: (item) => item.type },
+  {
+    label: 'Complete',
+    renderCell: (item) => item.isComplete.toString(),
+  },
+  { label: 'Tasks', renderCell: (item) => item.nodes },
+];
+
 
 function Preview () {
-  const tg = useTelegramWeb();
-  tg.MainButton.setParams({text: 'Закрыть', is_visible: true}).onClick(() => {
-    tg.close()
-  });
+//   const tg = useTelegramWeb();
+//   tg.MainButton.setParams({text: 'Закрыть', is_visible: true}).onClick(() => {
+//     tg.close()
+//   });
   
 //   const username = tg.initDataUnsafe.user.username;
-  const isAdmin = true //(username == "venshupo" || username == "danetuzh")
-
-
-  const [modalOpen, setModalOpen] = useState(false);
-  const [rows, setRows] = useState([
-    {
-      page: "Home",
-      description: "This is the main page of the website",
-      status: "live",
-    },
-    {
-      page: "About Us",
-      description: "This page has details about the company",
-      status: "draft",
-    },
-    {
-      page: "Pricing",
-      description: "Prices for different subscriptions",
-      status: "error",
-    },
-  ]);
-  const [rowToEdit, setRowToEdit] = useState(null);
-
-  const handleDeleteRow = (targetIndex) => {
-    setRows(rows.filter((_, idx) => idx !== targetIndex));
-  };
-
-  const handleEditRow = (idx) => {
-    setRowToEdit(idx);
-
-    setModalOpen(true);
-  };
-
-  const handleSubmit = (newRow) => {
-    rowToEdit === null
-      ? setRows([...rows, newRow])
-      : setRows(
-          rows.map((currRow, idx) => {
-            if (idx !== rowToEdit) return currRow;
-
-            return newRow;
-          })
-        );
-  };
-
-  return (
-    <div>
-    {isAdmin && 
-        <div
-        className={styles.preview}
-        style={{backgroundColor: 'var(--tg-theme-secondary-bg-color)'}}
-        >
-          <div className="App">
-            <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
-            <button onClick={() => setModalOpen(true)} className="btn">
-                Add
-            </button>
-            {modalOpen && (
-                <Modal
-                closeModal={() => {
-                    setModalOpen(false);
-                    setRowToEdit(null);
-                }}
-                onSubmit={handleSubmit}
-                defaultValue={rowToEdit !== null && rows[rowToEdit]}
-                />
-            )}
-          </div>
-        </div>
-    }
-    </div>
-  )
+//   const isAdmin = true //(username == "venshupo" || username == "danetuzh")
+  const data = { nodes };
+  return <CompactTable columns={COLUMNS} data={data} />;
+//   return (
+//     <div>
+//     {isAdmin && 
+//         <div
+//         className={styles.preview}
+//         style={{backgroundColor: 'var(--tg-theme-secondary-bg-color)'}}
+//         >
+//           <div className="App">
+//            <CompactTable columns={COLUMNS} data={data} />
+//           </div>
+//         </div>
+//     }
+//     </div>
+//   )
 }
 
 export default Preview
