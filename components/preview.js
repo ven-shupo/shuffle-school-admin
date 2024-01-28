ed3fcba6492d5a9e00495f9f5a989b44cb275492
opushnev <opushnev.ia@phystech.edu>
2024-01-28 18:42:03 +0300

    commit

--- components/preview.js
+++ components/preview.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react'
 import {useTelegramWeb} from "../lib/telegramWeb";
 import styles from '../styles/Home.module.css';
-
+import { Table } from "./table";
+import { Modal } from "./modal";
 
 function Preview () {
   const tg = useTelegramWeb();
@@ -9,8 +10,52 @@ function Preview () {
     tg.close()
   });
   
-  const username = tg.initDataUnsafe.user.username;
-  const isAdmin = (username == "venshupo" || username == "danetuzh")
+//   const username = tg.initDataUnsafe.user.username;
+  const isAdmin = true //(username == "venshupo" || username == "danetuzh")
+
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [rows, setRows] = useState([
+    {
+      page: "Home",
+      description: "This is the main page of the website",
+      status: "live",
+    },
+    {
+      page: "About Us",
+      description: "This page has details about the company",
+      status: "draft",
+    },
+    {
+      page: "Pricing",
+      description: "Prices for different subscriptions",
+      status: "error",
+    },
+  ]);
+  const [rowToEdit, setRowToEdit] = useState(null);
+
+  const handleDeleteRow = (targetIndex) => {
+    setRows(rows.filter((_, idx) => idx !== targetIndex));
+  };
+
+  const handleEditRow = (idx) => {
+    setRowToEdit(idx);
+
+    setModalOpen(true);
+  };
+
+  const handleSubmit = (newRow) => {
+    rowToEdit === null
+      ? setRows([...rows, newRow])
+      : setRows(
+          rows.map((currRow, idx) => {
+            if (idx !== rowToEdit) return currRow;
+
+            return newRow;
+          })
+        );
+  };
+
   return (
     <div>
     {isAdmin && 
@@ -18,13 +63,21 @@ function Preview () {
         className={styles.preview}
         style={{backgroundColor: 'var(--tg-theme-secondary-bg-color)'}}
         >
-          <div
-            className={styles.card}
-            style={{ 
-                backgroundImage: 'url(https://ven-shupo.github.io/shuffle-school-admin/card.png)',
-                backgroundColor: 'var(--tg-theme-secondary-bg-color)'
-            }}
-          >
+          <div className="App">
+            <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+            <button onClick={() => setModalOpen(true)} className="btn">
+                Add
+            </button>
+            {modalOpen && (
+                <Modal
+                closeModal={() => {
+                    setModalOpen(false);
+                    setRowToEdit(null);
+                }}
+                onSubmit={handleSubmit}
+                defaultValue={rowToEdit !== null && rows[rowToEdit]}
+                />
+            )}
           </div>
         </div>
     }
