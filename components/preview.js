9681dbaf493e2877876086ad02fc54667294e0fc
opushnev <opushnev.ia@phystech.edu>
2024-01-28 17:51:48 +0300

    check admin

--- components/preview.js
+++ components/preview.js
@@ -8,35 +8,19 @@ function Preview () {
   tg.MainButton.setParams({text: 'Закрыть', is_visible: true}).onClick(() => {
     tg.close()
   });
-  console.log("tg", tg)
-  console.log("tg.initDataUnsafe", tg.initDataUnsafe)
-  console.log("tg.initDataUnsafe.user", tg.initDataUnsafe.user)
-  console.log("tg.initDataUnsafe.user.username", tg.initDataUnsafe.user.username)
  
-  const [lessons, setLessons] = useState();
-
-  const requestOptions = {
-    method: 'GET',
-    headers: { "Authorization": "Bearer pattpUkpI0kiExoi9.e98cfe85447f4a5d49fbd63d0f59baa57121f7578e207036c666c8cb0329eeb9"},
-  };
-  const tgUserName = tg.initDataUnsafe.user.username;
-  var url = new URL("https://api.airtable.com/v0/appXfAFgufLXTHPVr/dancer");
-  var params = {filterByFormula: 'tg="' + tgUserName + '"'};
-  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
-  console.log("new url", url);
-  fetch(url, requestOptions)
-    .then((response) => response.json())
-    .then((data) => {
-        setLessons(data.records[0].fields.classes_left);
-    })
-    .catch((err) => {
-        console.log(err.message);
-  });
+  const [isAdmin, setIsAdmin] = useState();
+  const username = tg.initDataUnsafe.user.username;
+  if (username == "vanshupo" || username == "") {
+    setIsAdmin(true)
+  }
   return (
-    <div
+    <div>
+    {isAdmin && 
+        <div
         className={styles.preview}
         style={{backgroundColor: 'var(--tg-theme-secondary-bg-color)'}}
-    >
+        >
         <div
             className={styles.card}
             style={{ 
@@ -56,6 +40,8 @@ function Preview () {
                 Осталось {lessons} занятий
             </div>
         }
+        </div>
+    } 
     </div>
   )
 }
