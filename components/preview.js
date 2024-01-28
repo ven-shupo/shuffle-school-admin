582084a535b4d4bfa264dfa4a8d79fb742d7cc0e
opushnev <opushnev.ia@phystech.edu>
2024-01-28 23:02:41 +0300

    added fetch rows

--- components/preview.js
+++ components/preview.js
@@ -6,29 +6,51 @@ import styles from '../styles/Home.module.css';
 
 
 function Preview () {
+    const tg = useTelegramWeb();
+    tg.MainButton.setParams({text: 'Закрыть', is_visible: true}).onClick(() => {
+      tg.close()
+    });
+    
+    // const tgUserName = tg.initDataUnsafe.user.username;
+    const requestOptions = {
+      method: 'GET',
+      headers: { "Authorization": "Bearer pattpUkpI0kiExoi9.e98cfe85447f4a5d49fbd63d0f59baa57121f7578e207036c666c8cb0329eeb9"},
+    };
+    var url = new URL("https://api.airtable.com/v0/appXfAFgufLXTHPVr/dancer");
+    var params = {maxRecords: 30};
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+    const {rows, setRows} = useState([]);
+    fetch(url, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        newRows = [];
+        for (const dancer of data.records) {
+          newRows.push(
+            <input defaultValue={i} {...register('tg_' + dancer.id, {required: true})}/>
+          );
+          newRows.push(
+            <input defaultValue={i} {...register('left_' + dancer.id, { pattern: /\d+/, required: true})} />
+          );
+          newRows.push(<br/>)
+        }
+        newRows.push(
+          <input {...register('tg_new')}/>
+        );
+        newRows.push(
+          <input {...register('left_new', { pattern: /\d+/})} />
+        );
+        newRows.push(<br/>);
+        setRows(newRows);
+      })
+      .catch((err) => {
+          console.log(err.message);
+    });
+
     const {
         register,
         handleSubmit,
         formState: { errors },
     } = useForm();
-    const rows = [];
-    for (let i = 1; i <= 5; i++) {
-      rows.push(
-        <input defaultValue={i} {...register('tg_' + i, {required: true})}/>
-      );
-      rows.push(
-        <input defaultValue={i} {...register('left_' + i, { pattern: /\d+/, required: true})} />
-      );
-      rows.push(<br/>)
-    }
-
-    rows.push(
-      <input {...register('tg_new')}/>
-    );
-    rows.push(
-      <input {...register('left_new', { pattern: /\d+/})} />
-    );
-    rows.push(<br/>)
 
     return (
         <form onSubmit={handleSubmit((data) => console.log(data))}>
