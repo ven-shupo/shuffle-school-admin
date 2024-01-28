f2bf3fb0f43f9ed6b101d116af92268f4d174e64
opushnev <opushnev.ia@phystech.edu>
2024-01-28 22:31:26 +0300

    added br

--- components/preview.js
+++ components/preview.js
@@ -16,9 +16,11 @@ function Preview () {
         <form onSubmit={handleSubmit((data) => console.log(data))}>
           <label>First Name</label>
           <input {...register('firstName')} />
+          <br/>
           <label>Lasr Name</label>
           <input {...register('lastName', { required: true })} />
           {errors.lastName && <p>Last name is required.</p>}
+          <br/>
           <label>Age</label>
           <input {...register('age', { pattern: /\d+/ })} />
           {errors.age && <p>Please enter number for age.</p>}
