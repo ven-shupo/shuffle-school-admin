3ff2251311a19c5c18ec0e44be0084341eef034f
opushnev <opushnev.ia@phystech.edu>
2024-01-31 23:30:48 +0300

    form for view

--- components/preview.js
+++ components/preview.js
@@ -1,70 +1,70 @@
-import React, {useState} from 'react';
-import DataGrid from 'react-data-grid';
+import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import {useTelegramWeb} from "../lib/telegramWeb";
-import styles from '../styles/Home.module.css';
+// import styles from '../styles/Home.module.css';
 
-function makeNewRows (data, register) {
-  const newRows = [];
+function makeFormRows (data, register) {
+  if (!data) { return null };
+
+  const rows = [];
   for (const dancer of data.records) {
-    newRows.push(
+    rows.push(
       <input defaultValue={dancer.fields.tg} {...register('tg_' + dancer.id, {required: true})}/>
     );
-    newRows.push(
+    rows.push(
       <input defaultValue={dancer.fields.classes_left} {...register('left_' + dancer.id, { pattern: /\d+/, required: true})} />
     );
-    newRows.push(<br/>)
-  }
-  newRows.push(
+    rows.push(<br/>)
+  };
+  rows.push(
     <input {...register('tg_new')}/>
   );
-  newRows.push(
+  rows.push(
     <input {...register('left_new', { pattern: /\d+/})} />
   );
-  newRows.push(<br/>);
-  return newRows
-}
-
+  rows.push(<br/>);
+  return rows
+};
 
 function Preview () {
     const tg = useTelegramWeb();
     tg.MainButton.setParams({text: 'Закрыть', is_visible: true}).onClick(() => {
       tg.close()
     });
+    
     const {
       register,
       handleSubmit,
       formState: { errors },
-  } = useForm();
-
-    // const tgUserName = tg.initDataUnsafe.user.username;
-    const requestOptions = {
-      method: 'GET',
-      headers: { "Authorization": "Bearer pattpUkpI0kiExoi9.e98cfe85447f4a5d49fbd63d0f59baa57121f7578e207036c666c8cb0329eeb9"},
-    };
-    var url = new URL("https://api.airtable.com/v0/appXfAFgufLXTHPVr/dancer");
-    var params = {maxRecords: 30};
-    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
-    const {rows, setRows} = useState([]);
-    fetch(url, requestOptions)
-      .then((response) => response.json())
-      .then((data) => {
-        setRows(makeNewRows(data, register));
+    } = useForm();
+    
+    const [dancers, setDancers] = useState(null);
+    useEffect(() => {
+      fetch("https://api.airtable.com/v0/appXfAFgufLXTHPVr/dancer?maxRecords=30", {
+        method: 'GET',
+        headers: { "Authorization": "Bearer pattpUkpI0kiExoi9.e98cfe85447f4a5d49fbd63d0f59baa57121f7578e207036c666c8cb0329eeb9"},
       })
-      .catch((err) => {
-          console.log(err.message);
-    });
+      .then(response => response.json())
+      .then(data => setDancers(data))
+      .catch(error => console.error('Error fetching data:', error));
+    }, []);
 
-
-
+    const r = makeFormRows(dancers, register);
     return (
-        <form onSubmit={handleSubmit((data) => console.log(data))}>
-          <label>Telegram username    | Classes left</label>
-          <br/>
-          {rows}
-          <input type="submit" />
-        </form>
-      );
-}
+      <div>
+        {r ? (
+          <div>
+            <form onSubmit={handleSubmit((data) => console.log(data))}>
+              {r}
+              <input type="submit" />
+            </form>
+          </div>
+        ) : (
+          <p>Loading data...</p>
+          )
+        }
+      </div>
+    );
+};
 
 export default Preview
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import styles from '../styles/Home.module.css';
 
 
 export default function Home() {
+  // return <Preview/>
   return (
     <div
       className={styles.app}
